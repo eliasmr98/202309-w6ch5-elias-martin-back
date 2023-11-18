2f28f0695524a3a22679e9d058b5e49da9b99697
eliasmr98 <eliasmartinrivas@gmail.com>
2023-11-18 23:04:08 +0100

    Fixing a problem with te async function

--- src/controller/films.controller.ts
+++ src/controller/films.controller.ts
@@ -1,5 +1,23 @@
 import { Request, Response } from 'express';
-import { dataArray } from '../files.js';
+import fs from 'fs/promises';
+import { Film } from '../model/film.js';
+import { ObjectEncodingOptions } from 'fs';
+
+const fileName = './api/db.json';
+const codeOptions: ObjectEncodingOptions = {
+  encoding: 'utf-8',
+};
+export let dataArray: Film[] = [];
+export const readDataFile = async () => {
+  try {
+    const rawData = (await fs.readFile(fileName, codeOptions)) as string;
+    dataArray = JSON.parse(rawData).films || [];
+  } catch (error) {
+    console.log((error as Error).message);
+  }
+};
+
+readDataFile();
 
 export const getAll = (_req: Request, res: Response) => {
   res.json(dataArray);
