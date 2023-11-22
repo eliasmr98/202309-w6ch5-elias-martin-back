5bb0f36be6f351df78be2f439f38282d91a31691
eliasmr98 <eliasmartinrivas@gmail.com>
2023-11-22 23:08:52 +0100

    Try to test create method from films repo

--- src/repos/films/films.mongo.repo.test.ts
+++ src/repos/films/films.mongo.repo.test.ts
@@ -1,81 +1,92 @@
-import { Film } from '../../entities/film.js';
-import { FilmsMongoRepo } from './films.mongo.repo.js';
-import { HttpError } from '../../types/http.error.js';
+import { FilmsMongoRepo } from './films.mongo.repo';
 import { FilmModel } from './films.mongo.model.js';
+// Temp import { UsersMongoRepo } from '../users/users.mongo.repo.js';
+// Temp import { Film } from '../../entities/film.js';
 
 jest.mock('./films.mongo.model.js');
+// Temp jest.mock('../users/users.mongo.repo.js');
 
-describe('Given FilmsMongoRepo class', () => {
-  let mockId: string;
-  let mockName: string;
-  let mockData: Partial<Film>[];
-
+describe('Given FilmsMongoRepo', () => {
+  let repo: FilmsMongoRepo;
   beforeEach(() => {
-    mockId = '1';
-    mockName = 'Godzilla VS Goku';
-    mockData = [{ id: mockId, name: mockName }];
+    repo = new FilmsMongoRepo();
+  });
+  describe('When we isntantiate it without errors', () => {
+    const exec = jest.fn().mockResolvedValue('Test');
 
-    (FilmModel.find as jest.Mock).mockResolvedValue(mockData);
-    (FilmModel.findById as jest.Mock).mockResolvedValue(mockData[0]);
-    (FilmModel.create as jest.Mock).mockResolvedValue(mockData[0]);
-    (FilmModel.findByIdAndUpdate as jest.Mock).mockResolvedValue(mockData[0]);
-    (FilmModel.findByIdAndDelete as jest.Mock).mockResolvedValue(mockData[0]);
+    beforeEach(() => {
+      FilmModel.find = jest.fn().mockReturnValue({
+        populate: jest.fn().mockReturnValue({
+          exec,
+        }),
+      });
+
+      FilmModel.findById = jest.fn().mockReturnValue({
+        populate: jest.fn().mockReturnValue({
+          exec,
+        }),
+      });
+
+      // FilmModel.create = jest.fn().mockResolvedValue({
+      //   id: '1',
+      //   name: 'Test Film',
+      //   author: { id: '1' },
+      // });
+
+      // UsersMongoRepo.prototype.getById = jest
+      //   .fn()
+      //   .mockResolvedValue({ films: [], id: '1' }) as jest.Mock;
+      // UsersMongoRepo.prototype.update = jest
+      //   .fn()
+      //   .mockResolvedValue(undefined) as jest.Mock;
+    });
+
+    test('Then it should execute getAll', async () => {
+      const result = await repo.getAll();
+      expect(exec).toHaveBeenCalled();
+      expect(result).toBe('Test');
+    });
+
+    test('Then it should execute getById', async () => {
+      const result = await repo.getById('');
+      expect(exec).toHaveBeenCalled();
+      expect(result).toBe('Test');
+    });
+    // Temp test('Then it should execute create', async () => {
+    //   const newMockItem = {
+    //     name: 'Test Film',
+    //     author: { id: '1' },
+    //   } as unknown as Omit<Film, 'id'>;
+    //   const result = await repo.create(newMockItem);
+
+    //   expect(FilmModel.create as jest.Mock).toHaveBeenCalledWith(newMockItem);
+    //   expect(UsersMongoRepo.prototype.getById).toHaveBeenCalledWith('1');
+    //   expect(UsersMongoRepo.prototype.update).toHaveBeenCalledWith('1', {
+    //     id: '1',
+    //     films: ['1'],
+    //   });
+
+    //   expect(result).toEqual({
+    //     id: '1',
+    //     name: 'Test Film',
+    //     author: { id: '1', films: ['1'] },
+    //   });
+    // });
   });
 
-  describe('When we instantiate it without errors', () => {
-    test('Then getAll should return the list of films', async () => {
-      const repo = new FilmsMongoRepo();
-      const result = await repo.getAll();
-      expect(result).toEqual(mockData);
-    });
-
-    test('Then getById should return the film with the specified id', async () => {
-      const repo = new FilmsMongoRepo();
-      const result = await repo.getById(mockId);
-      expect(result).toEqual(mockData[0]);
-    });
-
-    test('Then getById should throw HttpError for non-existent id', async () => {
-      const repo = new FilmsMongoRepo();
-      const nonExistentId = 'non-existent-id';
-      (FilmModel.findById as jest.Mock).mockResolvedValue(null);
-      await expect(repo.getById(nonExistentId)).rejects.toThrow(HttpError);
-    });
-
-    test('Then create should add a new film and return it', async () => {
-      const repo = new FilmsMongoRepo();
-      const newFilm = { name: mockName } as Omit<Film, 'id'>;
-      const result = await repo.create(newFilm);
-      expect(result).toEqual(mockData[0]);
-    });
-
-    test('Then update should modify an existing film and return it', async () => {
-      const repo = new FilmsMongoRepo();
-      const updatedFilm = { name: mockName };
-      const result = await repo.update(mockId, updatedFilm);
-      expect(result).toStrictEqual({
-        id: mockId,
-        name: mockName,
+  describe('When we isntantiate it WITH errors', () => {
+    const exec = jest.fn().mockRejectedValue(new Error('Test'));
+    beforeEach(() => {
+      FilmModel.findById = jest.fn().mockReturnValue({
+        populate: jest.fn().mockReturnValue({
+          exec,
+        }),
       });
+      repo = new FilmsMongoRepo();
     });
 
-    test('Then update should throw HttpError for non-existent id', async () => {
-      const repo = new FilmsMongoRepo();
-      const nonExistentId = 'non-existent-id';
-      (FilmModel.findByIdAndUpdate as jest.Mock).mockResolvedValue(null);
-      await expect(repo.update(nonExistentId, {})).rejects.toThrow(HttpError);
-    });
-
-    test('Then delete should remove an existing film', async () => {
-      const repo = new FilmsMongoRepo();
-      await repo.delete(mockId);
-    });
-
-    test('Then delete should throw HttpError for non-existent id', async () => {
-      const repo = new FilmsMongoRepo();
-      const nonExistentId = 'non-existent-id';
-      (FilmModel.findByIdAndDelete as jest.Mock).mockResolvedValue(null);
-      await expect(repo.delete(nonExistentId)).rejects.toThrow(HttpError);
+    test('Then it should execute getById', async () => {
+      expect(repo.getById('')).rejects.toThrow();
     });
   });
 });
