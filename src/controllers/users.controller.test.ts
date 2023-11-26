ba06820d7a42850cd4e020ec1e25a117cc1e62de
eliasmr98 <eliasmartinrivas@gmail.com>
2023-11-26 21:05:10 +0100

    Try to fix duplicated code

--- src/controllers/users.controller.test.ts
+++ src/controllers/users.controller.test.ts
@@ -22,53 +22,6 @@ describe('Given FilmsController class', () => {
   });
 
   describe('When we instantiate it without errors', () => {
-    beforeEach(() => {
-      const mockRepo = {
-        getAll: jest.fn().mockResolvedValue([{}]),
-        getById: jest.fn().mockResolvedValue({}),
-        search: jest.fn().mockResolvedValue([{}]),
-        create: jest.fn().mockResolvedValue({}),
-        update: jest.fn().mockResolvedValue({}),
-        delete: jest.fn().mockResolvedValue(undefined),
-      } as unknown as UsersMongoRepo;
-
-      controller = new UsersController(mockRepo);
-    });
-
-    test('Then getAll should...', async () => {
-      await controller.getAll(mockRequest, mockResponse, mockNext);
-      expect(mockResponse.json).toHaveBeenCalledWith([{}]);
-    });
-
-    test('Then getById should...', async () => {
-      await controller.getById(mockRequest, mockResponse, mockNext);
-      expect(mockResponse.json).toHaveBeenCalledWith({});
-    });
-
-    test('Then search should...', async () => {
-      await controller.search(mockRequest, mockResponse, mockNext);
-      expect(mockResponse.json).toHaveBeenCalledWith([{}]);
-    });
-
-    test('Then create should...', async () => {
-      await controller.create(mockRequest, mockResponse, mockNext);
-      expect(mockResponse.status).toHaveBeenCalledWith(201);
-      expect(mockResponse.statusMessage).toBe('Created');
-      expect(mockResponse.json).toHaveBeenCalledWith({});
-    });
-
-    test('Then update should...', async () => {
-      await controller.update(mockRequest, mockResponse, mockNext);
-      expect(mockResponse.json).toHaveBeenCalledWith({});
-    });
-
-    test('Then delete should...', async () => {
-      await controller.delete(mockRequest, mockResponse, mockNext);
-      expect(mockResponse.status).toHaveBeenCalledWith(204);
-      expect(mockResponse.statusMessage).toBe('No Content');
-      expect(mockResponse.json).toHaveBeenCalledWith({});
-    });
-
     test('Then login should...', async () => {
       const mockUserId = 'mockUserId';
       const mockLoginResult = { id: 'mockUserId', email: 'mock@example.com' };
@@ -89,49 +42,13 @@ describe('Given FilmsController class', () => {
 
   describe('When we instantiate it with errors', () => {
     let mockError: Error;
-
     beforeEach(() => {
       mockError = new Error('Mock error');
       const mockRepo = {
-        getAll: jest.fn().mockRejectedValue(mockError),
-        getById: jest.fn().mockRejectedValue(mockError),
-        search: jest.fn().mockRejectedValue(mockError),
-        create: jest.fn().mockRejectedValue(mockError),
-        update: jest.fn().mockRejectedValue(mockError),
-        delete: jest.fn().mockRejectedValue(mockError),
         login: jest.fn().mockRejectedValue(mockError),
       } as unknown as UsersMongoRepo;
 
       controller = new UsersController(mockRepo);
-    });
-    test('Then getAll should...', async () => {
-      await controller.getAll(mockRequest, mockResponse, mockNext);
-      expect(mockNext).toHaveBeenCalledWith(mockError);
-    });
-
-    test('Then getById should...', async () => {
-      await controller.getById(mockRequest, mockResponse, mockNext);
-      expect(mockNext).toHaveBeenCalledWith(mockError);
-    });
-
-    test('Then search should...', async () => {
-      await controller.search(mockRequest, mockResponse, mockNext);
-      expect(mockNext).toHaveBeenCalledWith(mockError);
-    });
-
-    test('Then create should...', async () => {
-      await controller.create(mockRequest, mockResponse, mockNext);
-      expect(mockNext).toHaveBeenCalledWith(mockError);
-    });
-
-    test('Then update should...', async () => {
-      await controller.update(mockRequest, mockResponse, mockNext);
-      expect(mockNext).toHaveBeenCalledWith(mockError);
-    });
-
-    test('Then delete should...', async () => {
-      await controller.delete(mockRequest, mockResponse, mockNext);
-      expect(mockNext).toHaveBeenCalledWith(mockError);
     });
     test('Then login should...', async () => {
       await controller.login(mockRequest, mockResponse, mockNext);
