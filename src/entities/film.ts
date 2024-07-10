302306b6e2aaf829b17fe6a75d15fdc93cb8bfe1
Elias MARTIN <emartin@up-spain.com>
2024-07-10 10:02:22 +0200

    Add images an remove some schema properties

--- src/entities/film.ts
+++ src/entities/film.ts
@@ -4,11 +4,9 @@ import { User } from './user.js';
 export type Film = {
   id: string;
   name: string;
-  producer: string;
   era: string;
   year: string;
   filmFrontImg: ImgData;
-  filmBackImg: ImgData;
   director: string;
   author: User;
 };
